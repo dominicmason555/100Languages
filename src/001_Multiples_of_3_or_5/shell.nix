86588c48848195fc47a6e25ffc2f38b613ede3bf
Dominic Mason <dominicmason555@gmail.com>
2024-05-06 19:32:11 +0100

    Added Nix/Direnv integration

--- src/001_Multiples_of_3_or_5/shell.nix
+++ src/001_Multiples_of_3_or_5/shell.nix
@@ -4,7 +4,11 @@ pkgs.mkShell {
     name = "problem_1";
     nativeBuildInputs = with pkgs.buildPackages; [
         just
+        lazygit
         lua54Packages.lua
         lua54Packages.fennel
     ];
+    shellHook = ''
+        echo "Project Euler: Problem 1"
+    '';
 }
