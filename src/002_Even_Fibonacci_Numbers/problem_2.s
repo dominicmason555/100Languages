7b79b2ea43cd271035700ae9a5236843878f6d59
Dominic Mason <dominicmason555@gmail.com>
2024-05-17 13:00:20 +0100

    Bug hunting

--- src/002_Even_Fibonacci_Numbers/problem_2.s
+++ src/002_Even_Fibonacci_Numbers/problem_2.s
@@ -1,28 +1,54 @@
         .global main
 
-loop:
-        add     w0, w0, #1
-        subs    x1, x1, #1
-        bne     loop
+fact_start:
+        mov     x2, x0                  // Keep x0 and x1 free
+        mov     x3, #1                  // First term in x3
+        mov     x4, #2                  // Second term in x4
+        mov     x5, #2                  // Sum in x5
+
+fact_1:
+        add     x3, x3, x4              // Add terms, store in x3
+        tst     x3, #1                  // Test if lowest bit is high (odd)
+        beq     fact_1_end              // If odd, skip to 4 million test
+        add     x5, x5, x3              // Else, add term to sum
+
+fact_1_end:
+        subs    x6, x3, x2              // Subtract 4 million
+        bpl     fact_end
+
+fact_2:
+        add     x4, x4, x3              // Add terms, store in x4
+        tst     x4, #1                  // Test if lowest bit is high (odd)
+        beq     fact_2_end              // If odd, skip to 4 million test
+        add     x5, x5, x4              // Else, add term to sum
+
+fact_2_end:
+        subs    x6, x4, x2              // Subtract 4 million
+        bpl     fact_end
+        b       fact_1
+
+fact_end:
+        mov     x0, x5                  // Return sum
         ret
 
 main:
         stp     x29, x30, [sp, #-16]!   // Prepare stack
         mov     x29, sp
-                                        // Solve the problem, put it in w1
-        mov     w0, #3                  // Load Input
-        mov     w1, #2304               // Lower 16 bits of 4 million
-        movk    w1, #61, lsl #16        // Upper 16 bits of 4 million
-        bl      loop
-        mov     w1, w0
 
+        //mov     x0, #0x900              // Lower 16 bits of 4 million
+        //movk    x0, #0x3D, lsl #16      // Upper 16 bits of 4 million
+        mov     x0, #46000
+        bl      fact_start              // Call factorial
+
+        mov     x1, x0                  // Move result to be printed
         adrp    x0, .L.str.ans          // Load format string and call printf
         add     x0, x0, :lo12:.L.str.ans
         bl      printf
 
-        mov     w0, wzr                 // Zero w0 to return 0 from main
+        mov     x0, xzr                 // Zero x0 to return 0 from main
         ldp     x29, x30, [sp], #16     // Clean stack
         ret                             // Return 0
 
 .L.str.ans:
-        .asciz  "Answer: %d\n"
+        .asciz  "Answer: %ld\n"
+
